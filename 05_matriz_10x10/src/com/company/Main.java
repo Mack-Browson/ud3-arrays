c40d13a6d10af1af61c15e7e9ea2772aca96ede3
Unknown <obybrown65@gmail.com>
2018-02-20 00:20:26 +0100

    Matriz de orden 10

--- 05_matriz_10x10/src/com/company/Main.java
+++ 05_matriz_10x10/src/com/company/Main.java
@@ -1,52 +1,31 @@
 package com.company;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 	// write your code here
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int[][] matriz=new int[10][10];
 
-         // Recibir datos desde el teclado
+        Random r =new Random();
+        int aleatorio=r.nextInt(10);
 
-        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
-        // numeros aleatorios
-
-        Random r = new Random ();
-
-        int aleatorio = r.nextInt(100);
-
-        System.out.println (" cuantas filas quieres en esa matriz ");
-        int fila = Integer.parseInt (br.readLine ());
-
-        System.out.println (" Dame ahora el numero de columnas ");
-        int columnas = Integer.parseInt (br.readLine ());
-
-        int[][] matriz = new int[fila][columnas];
-
-
-        for (int i= 0; i<matriz.length;i++){
-            for (int j=0; j<matriz[i].length;j++){
-               // System.out.print ( " valores aleatorios para la matriz : ");
-                matriz[i][j]= Integer.parseInt (String.valueOf (aleatorio));
+        for (int i=0;i<matriz.length;i++){
+            for (int j=0; j<matriz.length;j++){
+                matriz[i][j]=aleatorio;
             }
         }
 
-        //  mostrar las matrices
-
-
-        for (int i= 0; i<matriz.length; i++){
-
-            for (int j=0; j<matriz[i].length;j++){
-
+        //Mostrar
+        for (int i=0;i<matriz.length;i++){
+            for (int j=0;j<matriz.length;j++){
                 System.out.print(matriz[i][j]);
             }
-            System.out.println (" ");
+            System.out.println();
         }
-
-        // No se porque me imprime la columna el doble
     }
 }
